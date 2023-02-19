5a2d1a72bd64ae33659dfee5563fa2fa4df5c9d6
Ganapathi <ganapa3689@gmail.com>
2023-02-19 11:06:10 +0530

    dashboard ,courses,lessons

---
+++ internship/admininclude/header.php
@@ -0,0 +1,22 @@
+<!Doctype HTML>
+<html>
+<head>
+	<title>ADMIN</title>
+	<link rel="stylesheet" href="dashboard.css" type="text/css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+
+<body>
+	
+	<div id="mySidenav" class="sidenav">
+	<p class="logo"><span>E</span>du-tech</p>
+	<a href="dashboard.php" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
+	<a href="courses.php"class="icon-a"><i class="fa fa-book" style="font-size:24px"></i></i> &nbsp;&nbsp;Courses</a>
+	<a href="#"class="icon-a"><i class="fa fa-youtube-play" style="font-size:24px"></i> &nbsp;&nbsp;Lectures</a>
+	<a href="#"class="icon-a"><i class="fa fa-mortar-board" style="font-size:24px"></i> &nbsp;&nbsp;Quiz</a>
+	<a href="#"class="icon-a"><i class="fa fa-credit-card" style="font-size:24px"></i> &nbsp;&nbsp;payment</a>
+	<a href="#"class="icon-a"><i class="fa fa-key" style="font-size:24px"></i> &nbsp;&nbsp;Change password</a>
+	<a href="#"class="icon-a"><i class="fa fa-sign-out" style="font-size:24px"></i> &nbsp;&nbsp;Logout</a>
+
+</div>
---
+++ internship/popup.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener('click',function() {
+    document.querySelector('.bg-model').style.display = 'block';
+});
+document.querySelector('.close').addEventListener('click',function() {
+    document.querySelector('.bg-model').style.display = 'none';
+});
